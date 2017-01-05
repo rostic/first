305266bb38ff3eb80fe5d1415e94bccef4e50f3e
Rostik <rkvitnickiy@mail.ru>
2017-01-06 01:05:11 +0200

    Commit 2.2

--- getForm.php
+++ getForm.php
@@ -4,13 +4,13 @@
 	$squares=$_POST['s'];
 	//connect db
 	$connect = mysql_connect("localhost","root","")or die(mysql_error());
-	mysql_select_db("database");
+	mysql_select_db("mysite");
 	mysql_query("SET NAMES 'cp1251';");
 
-	$s="CREATE TABLE IF NOT EXISTS flat (num int, name char(30), squares int, sellerId int);";
+	$s="CREATE TABLE IF NOT EXISTS flat (id int PRIMARY KEY AUTO_INCREMENT, num int, name char(30), squares int);";
 	mysql_query($s);
 	$s="INSERT INTO flat VALUES
-	(".$num.",'".$name."','".$squares."','2');";
+	(null,".$num.",'".$name."','".$squares."');";
 	mysql_query($s);
 
 	// $s="SELECT * FROM flat;";
